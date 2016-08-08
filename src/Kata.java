ae419e71ec6366a4efb4872e938482ae695b2356
dave hochstrasser <davehochstrasser@daves-MacBook-Pro.local>
2016-08-08 17:59:03 -0400

    First Commit

---
+++ src/Kata.java
@@ -0,0 +1,133 @@
+import java.util.Scanner;
+
+/**
+ * Created by davehochstrasser on 8/8/16.
+ */
+public class Kata {
+
+    //public boolean sleepIn(boolean weekday, boolean vacation) {
+
+    //return (!weekday || vacation);
+
+
+    //public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+    //monkeyTrouble(true, true) → true
+    //monkeyTrouble(false, false) → true
+    //monkeyTrouble(true, false) → false
+    //return ((aSmile && bSmile) || (!aSmile && !bSmile));
+    //}
+    //sumDouble(1, 2) → 3
+    //sumDouble(3, 2) → 5
+    //sumDouble(2, 2) → 8
+    //public int sumDouble(int a, int b) {
+    //   int sum = (a + b);
+    // if(a == b) {
+    //   return (sum * 2);
+    //}
+    //else {
+    //  return(sum);
+    //}
+
+    //}
+    //}
+    //diff21(19) → 2
+    //diff21(10) → 11
+    //diff21(21) → 0
+
+    //public int diff21(int n) {
+    //int diff = (21 - n);
+    //if (n >= 21) {
+    //    return (diff * 2);
+    //  }
+
+    //else {
+    //    return (diff);
+    // }
+    //}
+    // parrotTrouble(true, 6) → true
+    //parrotTrouble(true, 7) → false
+    //parrotTrouble(false, 6) → false
+
+    //public boolean parrotTrouble(boolean talking, int hour) {
+    //if ((talking == true) &&(hour < 7 || hour > 20 )) {
+    // return (true);
+    //}
+    // else{
+    // return(false);
+    //makes10(9, 10) → true
+    //makes10(9, 9) → false
+    //makes10(1, 9) → true
+
+
+    //public boolean makes10(int a, int b) {
+    //if ((a ==10 || b == 10) || (a + b == 10)) {
+    // return (true);
+    //}
+    // else{
+    // return(false);
+
+    //}
+    // }
+    //nearHundred(93) → true
+    //nearHundred(90) → true
+    //nearHundred(89) → false
+    //public boolean nearHundred(int n) {
+    //    if (((100 + n <= Math.abs(10)) || (100 - n <= Math.abs(10))
+    //|| ((200 + n <= Math.abs(10)) || (200 - n <= Math.abs(10)))))
+    //        return (true);
+    //else{
+    //        return(false);
+
+    //    }
+    //posNeg(1, -1, false) → true
+    //posNeg(-1, 1, false) → true
+    //posNeg(-4, -5, true) → true
+    public boolean posNeg(int a, int b, boolean negative) {
+
+        if (((a < 0) && (b > 0) || ((a > 0) && (b < 0)))) {
+            return (true);
+        }
+        if (((a < 0) && (b < 0)) && (negative == true)) {
+            return (true);
+        }
+        return (false);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+/**
+ * Created by davehochstrasser on 8/8/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        //Scanner scanner = new Scanner(System.in);
+        //System.out.println("Give me a number: ");
+
+        //int x = scanner.nextInt();
+
+        Kata kata = new Kata();
+
+
+        //System.out.println( kata.sleepIn(false, false) );
+        //System.out.println(kata.sleepIn(true, false) );
+        //System.out.println(kata.sleepIn(false, true) );
+
+        //System.out.println( kata.monkeyTrouble(false, false) );
+        //System.out.println(kata.monkeyTrouble(true, true) );
+        //System.out.println(kata.monkeyTrouble(false, true) );
+        //System.out.println(kata.sumDouble(1,2));
+        //System.out.println(kata.sumDouble(3,2));
+        //System.out.println(kata.sumDouble(2,2));
+        //System.out.println(kata.diff21(19));
+        //System.out.println(kata.diff21(10));
+        //System.out.println(kata.diff21(21));
+        //System.out.println(kata.parrotTrouble(true, 6));
+        //System.out.println(kata.parrotTrouble(true, 7));
+        //System.out.println(kata.parrotTrouble(false, 6));
+        //System.out.println(kata.makes10(9, 10));
+        //System.out.println(kata.makes10(9, 9));
+        //System.out.println(kata.makes10(1, 9));
+        //System.out.println(kata.nearHundred(93));
+        //System.out.println(kata.nearHundred(90));
+        //System.out.println(kata.nearHundred(89));
+        System.out.println(kata.posNeg(1, -1, false));
+        System.out.println(kata.posNeg(-1, 1, false));
+        System.out.println(kata.posNeg(-4, -5, true));
+
+
+
+    }
+
+}
